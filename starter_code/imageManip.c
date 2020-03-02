b73d9479c4236ba94802a98811c7c9649695f97a
Wilson Tjoeng <wtjoeng1@jhu.edu>
2020-03-02 10:05:32 -0500

    c file to implement image manipulation algorithms

---
+++ starter_code/imageManip.c
@@ -0,0 +1,8 @@
+// 1. exposure - change exposure of an image
+// 2. a-blend - blend two images by a given ratio
+// 3. zoom in - zoom in to an image
+// 4. zoom out - zoom out of an image
+// 5. pointilism - pointilism technique
+// 6. swirl - swirl the image
+// 7. blur - blur the image (smooth and soften its edges) by a specified amount
+// Wilson (1-3), Anire (4-6)
