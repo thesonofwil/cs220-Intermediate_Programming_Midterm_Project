bc20cec210d2061e07ad9ac32c57b16f55aaf3a2
Wilson Tjoeng <wtjoeng1@jhu.edu>
2020-03-06 17:28:25 -0500

    Made separate Methods directory

---
+++ starter_code/Methods/change_exposure.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ #include "ppm_io.c"
+ #include "ppm_io.h"
+ 
+ 
+ // struct pixel defined in header file
+ // struct image deinfed in header file
+ // Loops through an image and returns a list of rgb values separately
+ int get_pixel(FILE* image, int x, int y) {
+ 
+ }
+ 
+ // Shift the exposure of the image by a factor of val
+ void change_exposure(Image *img, float val) {
