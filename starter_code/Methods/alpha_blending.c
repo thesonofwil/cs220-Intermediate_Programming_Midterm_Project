4abb76d07035e3f0dbae4a3393aaaf7c471b4d49
aegbe2 <lax18christian@gmail.com>
2020-03-12 06:43:22 -0400

    working on alpha blend fcn 2

--- starter_code/Methods/alpha_blending.c
+++ starter_code/Methods/alpha_blending.c
@@ -24,11 +24,55 @@ void alpha_blending(Image *Image1, Image *Image2, float alpha){
     *(Image1.data + i).b = *(Image1.data + i).b * alpha;
   }
   // Changing Image 2 by 1 - alpha
-  numpix1 = Image1->rows * Image1->cols; //number of pixels in Image 1
+  numpix2 = Image2->rows * Image2->cols; //number of pixels in Image 1
   //create a loop to change each R,G,B value in each pixel
   float alpha1 = 1 - alpha;
-  for(int i = 0; i < numpix1; i++){
-    *(Image2.data + i).r = *(Image2.data + i).r * alpha1;
-    *(Image2.data + i).g = *(Image2.data + i).g * alpha1;
-    *(Image2.data + i).b = *(Image2.data + i).b * alpha1;
+  for(int j = 0; i < numpix2; i++){
+    *(Image2.data + j).r = *(Image2.data + j).r * alpha1;
+    *(Image2.data + j).g = *(Image2.data + j).g * alpha1;
+    *(Image2.data + j).b = *(Image2.data + j).b * alpha1;
   }
+  //create output Image
+
+  Image *output = (Image*) malloc(sizeof(Image));
+  //adds all the Image2 data to Image1
+  if(Image1.rows >= Image2.rows){
+    for(int k = 0; k < Image2.rows){
+      *(Image1.data + k).r = *(Image1.data + k).r + *(Image2.data + k).r;
+      //make sure max values for R is 255
+      if( *(Image1.data + k).r > 255){
+	*(Image1.data + k).r = 255;
+      }
+      *(Image1.data + k).g = *(Image1.data + k).g + *(Image2.data + k).g;
+      //make sure max value for G is 255
+      if( *(Image1.data + k).g > 255){
+        *(Image1.data + k).g = 255;
+      }
+      *(Image1.data + k).b = *(Image1.data + k).b + *(Image2.data + k).b;
+      //make sue max value for B is 255
+      if( *(Image1.data + k).b > 255){
+        *(Image1.data + k).b = 255;
+      }
+    } 
+    else {
+      for(int l = 0; l < Image1.rows){
+        *(Image2.data + l).r = *(Image2.data + l).r + *(Image1.data + l).r;
+        //make sue max value for R is 255
+        if( *(Image2.data + l).r > 255){
+          *(Image2.data + l).r = 255;
+        }
+        *(Image2.data + l).g = *(Image2.data + l).g + *(Image1.data + l).g;
+        //make sue max value for G  is 255
+        if( *(Image2.data + l).g > 255){
+          *(Image2.data + l).g = 255;
+        }
+        *(Image2.data + l).b = *(Image2.data + l).b + *(Image1.data + l).b;
+        //make sue max value for B is 255
+        if( *(Image2.data + k).b > 255){
+        *(Image2.data + k).b = 255;
+        }
+      }
+    }//ends else statment
+  }//ends if statement
+   
+
