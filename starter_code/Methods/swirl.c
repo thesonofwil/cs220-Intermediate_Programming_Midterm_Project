7ae18c15559ff03578f8ce1001f1dfa2bba2184f
Marcelo <lmoral10@jhu.edu>
2020-03-19 20:18:08 -0400

    worked on function to swirl pixels in image

--- starter_code/Methods/swirl.c
+++ starter_code/Methods/swirl.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+q#include <stdio.h>
 #include <stdlib.h>
 #include "ppm_io.c"
 #include "ppm_io.h"
 
 void swirl (Image * img) {
-
+  //having toruble which pizels to pick, how do i convey this through code
   int xCenter;
   int yCenter;
+
+  
   
 }
