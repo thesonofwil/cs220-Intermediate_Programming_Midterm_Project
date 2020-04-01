af13c1589736f2cd2bcc2f91fcf1e73ea92a22eb
aegbe2 <lax18christian@gmail.com>
2020-04-01 16:36:33 -0400

    fixed -> error in zoom out

--- starter_code/Final/zoom_out.c
+++ starter_code/Final/zoom_out.c
@@ -20,20 +20,20 @@ Image * zoom_out(Image *img) {
   for (int oRow = 0; oRow < output->rows; oRow++) {
     for (int oCol = 0; oCol < output->cols; oCol++) {
       //adds all the r values from the four pixels in the original image to the pixel in the output image
-      *(output.data + (oRow*output->cols + oCol)).r = *(img.data + (oRow*output->cols + oCol)).r + *(img.data + (oRow*output->cols + (oCol+1))).r
-	+ *(img.data + ( (oRow+1)*output->cols + (oCol+1))).r +   *(img.data + ( (oRow+1)*output->cols +  oCol)).r;
+      *(output->data + (oRow*output->cols + oCol)).r = *(img->data + (oRow*output->cols + oCol)).r + *(img->data + (oRow*output->cols + (oCol+1))).r
+	+ *(img->data + ( (oRow+1)*output->cols + (oCol+1))).r +   *(img->data + ( (oRow+1)*output->cols +  oCol)).r;
       //take average of the four r pixels
-      *(output.data + (oRow*output->cols + oCol)).r = *(output.data + (oRow*output->cols + oCol)).r /4;
+      *(output->data + (oRow*output->cols + oCol)).r = *(output->data + (oRow*output->cols + oCol)).r /4;
       //adds all the g values from the four pixels in the original image to the pixel in the output image
-      *(output.data + (oRow*output->cols + oCol)).g = *(img.data + (oRow*output->cols + oCol)).g + *(img.data + (oRow*output->cols + (oCol+1))).g
-        + *(img.data + ( (oRow+1)*output->cols + (oCol+1))).g +   *(img.data + ( (oRow+1)*output->cols +  oCol)).g;
+      *(output->data + (oRow*output->cols + oCol)).g = *(img->data + (oRow*output->cols + oCol)).g + *(img->data + (oRow*output->cols + (oCol+1))).g
+        + *(img->data + ( (oRow+1)*output->cols + (oCol+1))).g +   *(img->data + ( (oRow+1)*output->cols +  oCol)).g;
       //take average of the four g pixels
-      *(output.data + (oRow*output->cols + oCol)).g = *(output.data + (oRow*output->cols + oCol)).g /4;
+      *(output->data + (oRow*output->cols + oCol)).g = *(output->data + (oRow*output->cols + oCol)).g /4;
       //adds all the b values from the four pixels in the original image to the pixel in the output image
-      *(output.data + (oRow*output->cols + oCol)).b = *(img.data + (oRow*output->cols + oCol)).b + *(img.data + (oRow*output->cols + (oCol+1))).b
-        + *(img.data + ( (oRow+1)*output->cols + (oCol+1))).b +   *(img.data + ( (oRow+1)*output->cols +  oCol)).b;
+      *(output->data + (oRow*output->cols + oCol)).b = *(img->data + (oRow*output->cols + oCol)).b + *(img->data + (oRow*output->cols + (oCol+1))).b
+        + *(img->data + ( (oRow+1)*output->cols + (oCol+1))).b +   *(img->data + ( (oRow+1)*output->cols +  oCol)).b;
       //take average of the four b pixels
-      *(output.data + (oRow*output->cols + oCol)).b = *(output.data + (oRow*output->cols + oCol)).b /4;
+      *(output->data + (oRow*output->cols + oCol)).b = *(output->data + (oRow*output->cols + oCol)).b /4;
     }
   }
 
