030b8803389672301aa9ef2fe47774ed12135adf
aegbe2 <lax18christian@gmail.com>
2020-04-01 17:56:01 -0400

    new changes to zoom out

--- starter_code/Final/zoom_out.c
+++ starter_code/Final/zoom_out.c
@@ -19,8 +19,19 @@ Image * zoom_out(Image *img) {
   //creating output image
   for (int oRow = 0; oRow < output->rows; oRow++) {
     for (int oCol = 0; oCol < output->cols; oCol++) {
-      //adds all the r values from the four pixels in the original image to the pixel in the output image
-      *(output->data + (oRow*output->cols + oCol)).r = *(img->data + (oRow*output->cols + oCol)).r + *(img->data + (oRow*output->cols + (oCol+1))).r
+      //adds all the r values from the four pixels in the original image to the pixel in the output image and takes average
+      output->data[oRow*cols+oCol].r = (img->data[oRow*cols+oCol].r + img->data[oRow*cols+oCol+1].r +  img->data[(oRow+1)*cols+oCol].r +
+					img->data[(oRow+1)*cols+oCol+1].r) /4;
+      //adds all the b values from the four pixels in the original image to the pixel in the output image and takes average
+
+      output->data[oRow*cols+oCol].b = (img->data[oRow*cols+oCol].b + img->data[oRow*cols+oCol+1].b +  img->data[(oRow+1)*cols+oCol].b +
+                                        img->data[(oRow+1)*cols+oCol+1].b) /4;
+      //adds all the g values from the four pixels in the original image to the pixel in the output image and takes average
+
+      output->data[oRow*cols+oCol].g = (img->data[oRow*cols+oCol].g + img->data[oRow*cols+oCol+1].g +  img->data[(oRow+1)*cols+oCol].g +
+                                        img->data[(oRow+1)*cols+oCol+1].g) /4;
+
+      /* *(output->data + (oRow*output->cols + oCol)).r = *(img->data + (oRow*output->cols + oCol)).r + *(img->data + (oRow*output->cols + (oCol+1))).r
 	+ *(img->data + ( (oRow+1)*output->cols + (oCol+1))).r +   *(img->data + ( (oRow+1)*output->cols +  oCol)).r;
       //take average of the four r pixels
       *(output->data + (oRow*output->cols + oCol)).r = *(output->data + (oRow*output->cols + oCol)).r /4;
@@ -33,7 +44,7 @@ Image * zoom_out(Image *img) {
       *(output->data + (oRow*output->cols + oCol)).b = *(img->data + (oRow*output->cols + oCol)).b + *(img->data + (oRow*output->cols + (oCol+1))).b
         + *(img->data + ( (oRow+1)*output->cols + (oCol+1))).b +   *(img->data + ( (oRow+1)*output->cols +  oCol)).b;
       //take average of the four b pixels
-      *(output->data + (oRow*output->cols + oCol)).b = *(output->data + (oRow*output->cols + oCol)).b /4;
+      *(output->data + (oRow*output->cols + oCol)).b = *(output->data + (oRow*output->cols + oCol)).b /4;*/
     }
   }
 
