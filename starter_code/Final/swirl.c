80384d56b635301df0d2c4d547ac0b17a97ea4a3
Marcelo <lmoral10@jhu.edu>
2020-04-02 12:49:07 -0400

    fixed bug with swirl

--- starter_code/Final/swirl.c
+++ starter_code/Final/swirl.c
@@ -10,29 +10,30 @@
 
 //two inputs provided by user of a center (x, y) and a distortion scale are finction parameters
 void swirl (Image *img, int x_center, int y_center, int scale) {
+  printf("check first\n");
+  //definitng all my variables that will be used
 
-  //definitng all my variables that will be used, initial values will soon be changed
-  int columns_of_image = 0;
-  int rows_of_image = 0;
-  int x_coord = 0;
-  int y_coord = 0;
-  int delta_x = 0;
-  int delta_y = 0;
-  int new_x_coord = 0;
-  int new_y_coord = 0;
-  columns_of_image = columns_of_image;
-  rows_of_image = img -> rows;
+  int x_coord;
+  int y_coord;
+  int delta_x;
+  int delta_y;
+  int new_x_coord;
+  int new_y_coord;
+  
+  double alpha;
+  double cos_a;
+  double sin_a;
+  
+  int columns_of_image = img->cols;
+  int rows_of_image = img -> rows;
 
-  double alpha = 0;
-  double cos_a = 0;
-  double sin_a = 0;
+ 
 
   //array of pixels that will be modified 
   Pixel *array_of_pixels = malloc(sizeof(Pixel) * rows_of_image * columns_of_image);
 
   for (int current_row= 0; current_row < rows_of_image; ++current_row) {
     for (int current_column = 0; current_column < columns_of_image; ++current_column) {
-
       //array of pixels initially has all the same red, green, and blue channels as the image
       array_of_pixels[(current_row * columns_of_image) + current_column].r = img->data[(current_row * columns_of_image) + current_column].r;
       array_of_pixels[(current_row * columns_of_image) + current_column].g = img->data[(current_row * columns_of_image) + current_column].g;
@@ -48,7 +49,6 @@ void swirl (Image *img, int x_center, int y_center, int scale) {
 
   for (x_coord = 0; x_coord < columns_of_image; ++x_coord) {
     for (y_coord = 0; y_coord < rows_of_image; ++y_coord) {
-
       //implement swirl effect
       delta_x = x_coord - x_center;
       delta_y = y_coord - y_center;
@@ -62,7 +62,8 @@ void swirl (Image *img, int x_center, int y_center, int scale) {
 
       //check to make sure new location of pixel is in the dimensions of the image
       if (new_x_coord >= 0 && new_y_coord >= 0 && new_x_coord < columns_of_image && new_y_coord < rows_of_image) {
-        
+   
+	
         //changing location of pixel from old coordinates to new coordinates
         img->data[(y_coord * columns_of_image) + x_coord].r = array_of_pixels[(new_y_coord * columns_of_image) + new_x_coord].r;
         img->data[(y_coord * columns_of_image) + x_coord].g = array_of_pixels[(new_y_coord * columns_of_image) + new_x_coord].g;
